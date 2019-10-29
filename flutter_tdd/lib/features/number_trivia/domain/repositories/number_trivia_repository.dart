70d09586783833bde8eead8ca5e5621d05a26aa5
KeHongWei <1844581560@qq.com>
2019-10-29 21:30:39 +0800

    项目架构

---
+++ flutter_tdd/lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_tdd/core/error/failures.dart';
+import 'package:flutter_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
---
+++ flutter_tdd/lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_tdd/core/usecases/usecase.dart';
+import 'package:meta/meta.dart';
+import 'package:flutter_tdd/core/error/failures.dart';
+import 'package:flutter_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
+  final NumberTriviaRepository repository;
+
+  GetRandomNumberTrivia(this.repository);
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) async {
+    return await repository.getRandomNumberTrivia();
+  }
+}
